2c6b24dfa61ab5ed539f813a6f51aaa566e96d06
Dipu <dngldpu@gmail.com>
2022-09-04 14:14:39 +1000

    todo env

--- api/src/config/dbConfig.js
+++ api/src/config/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnect = () => {
   try {
-    const conn = mongoose.connect(process.env.MONGO_CLIENT);
+    const conn = process.env.MONGO_CLIENT && mongoose.connect(process.env.MONGO_CLIENT);
     conn && console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
